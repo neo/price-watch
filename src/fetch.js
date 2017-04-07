3337291e2aef7385dd452a6312ed04545bc1d3b9
Neo <neolwc@gmail.com>
2017-04-06 21:38:35 -0400

    Add some error handlings

--- src/fetch.js
+++ src/fetch.js
@@ -3,11 +3,18 @@ const jsdom = require('jsdom');
 module.exports = function(item) {
   return new Promise((resolve, reject) => {
     jsdom.env(item.url, (error, window) => {
-      if (error) reject(error);
+      if (error) {
+        reject(error);
+        return;
+      }
+      if (!window) {
+        reject('undefined window');
+        return;
+      }
       const priceTag = window.document.querySelector(item.selector);
       resolve({
         name: item.name || window.document.title,
-        price: priceTag ? priceTag.innerHTML.trim() : 'Invalid selector',
+        price: priceTag ? priceTag.innerHTML.trim() : 0,
         url: item.url
       });
     });
